07748e2a527b53d2af0efbebc8654d418a761f61
Zhenyatta <jenya.paniryan8@gmail.com>
2023-01-20 10:18:22 +0400

    first commit

--- back-end/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>yaay this is something hosted from backend</h1>");
-});
-
-app.listen(8080);
