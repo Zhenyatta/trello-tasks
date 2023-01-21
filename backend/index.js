0449bbb1311a63eba278b4048d9c80e8b12087a4
Zhenyatta <jenya.paniryan8@gmail.com>
2023-01-21 10:16:06 +0400

    whitout-express-session

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,34 @@
-import express from "express";
-import session from "express-session";
 import fs from "fs";
+import express from "express";
 
 const app = express();
 
+let sessionCounter = 0;
 let counter;
 
-fs.readFile("temp/my-counter-visited-times.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  } else {
-    counter = Number(data);
-  }
-});
-
-app.use(
-  session({
-    secret: "secretkey",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+try {
+  fs.existsSync("temp/my-counter-visited-times.txt")
+    ? (counter = fs.readFileSync("temp/my-counter-visited-times.txt", "utf8"))
+    : (counter = 0);
+} catch (e) {
+  console.log(e);
+}
 
 app.get("/", (req, res) => {
-  req.session.views = (req.session.views || 0) + 1;
+  sessionCounter++;
+  counter = parseInt(counter);
   res.send(
-    `This page was visited ${counter + req.session.views} times in total,  ${
-      req.session.views
-    } out of which was during the current code execution session`
+    `This page was visited ${
+      counter + sessionCounter
+    } times in total, ${sessionCounter} out of which was during the current code execution session ( statuscode:${
+      res.statusCode
+    } ) `
   );
-  let str = JSON.stringify(counter + req.session.views);
+
+  let str = JSON.stringify(counter + sessionCounter);
   fs.writeFile("temp/my-counter-visited-times.txt", str, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("File written successfully");
-    }
+    if (err) throw err;
+    console.log("The file has been saved!");
   });
 });
 
