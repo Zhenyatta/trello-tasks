a18e47dfc33905f517e168078cbf6f758b907cbc
Zhenyatta <jenya.paniryan8@gmail.com>
2023-01-22 13:50:57 +0400

    revision changes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 import fs from "fs";
 import express from "express";
 
+const MY_COUNTER_VISITED_TIMES_FILE_PATH = 'temp/my-counter-visited-times.txt'
+
 const app = express();
 
 let pageVisitedTimesSession = 0;
 let pageVisitedTimesTotal = 0;
 
 try {
-  if (fs.existsSync("temp/my-counter-visited-times.txt")) {
-    (pageVisitedTimesTotal = Number(fs.readFileSync("temp/my-counter-visited-times.txt", "utf8")))
+  if (fs.existsSync(MY_COUNTER_VISITED_TIMES_FILE_PATH)) {
+    pageVisitedTimesTotal = Number(fs.readFileSync(MY_COUNTER_VISITED_TIMES_FILE_PATH, "utf8"))
   }
 } catch (e) {
   console.log(e);
@@ -16,14 +18,15 @@ try {
 
 app.get("/", (req, res) => {
   ++pageVisitedTimesSession;
+  ++pageVisitedTimesTotal;
 
   res.status(200).send(
-    `This page was visited ${++pageVisitedTimesTotal
+    `This page was visited ${pageVisitedTimesTotal
     } times in total, ${pageVisitedTimesSession} out of which was during the current code execution session ) `
   );
 
   try {
-    fs.writeFileSync("temp/my-counter-visited-times.txt", String(pageVisitedTimesTotal));
+    fs.writeFileSync(MY_COUNTER_VISITED_TIMES_FILE_PATH, String(pageVisitedTimesTotal));
   } catch (e) {
     console.log(e);
   }
