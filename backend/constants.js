fd23107b2f99e78c1c7e2ccd79f1b0c700dd31b9
Zhenya :) <79196310+Zhenyatta@users.noreply.github.com>
2023-01-23 16:20:44 +0400

    prettier config added (#4)

---
+++ backend/constants.js
@@ -0,0 +1,1 @@
+export const VISIT_COUNTER_FILE_PATH = 'temp/my-counter-visited-times.txt';
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-import fs from "fs";
-import express from "express";
+import fs from 'fs';
+import express from 'express';
 
-const MY_COUNTER_VISITED_TIMES_FILE_PATH = 'temp/my-counter-visited-times.txt'
+const VISIT_COUNTER_FILE_PATH = 'temp/my-counter-visited-times.txt';
 
 const app = express();
 
@@ -9,24 +9,26 @@ let pageVisitedTimesSession = 0;
 let pageVisitedTimesTotal = 0;
 
 try {
-  if (fs.existsSync(MY_COUNTER_VISITED_TIMES_FILE_PATH)) {
-    pageVisitedTimesTotal = Number(fs.readFileSync(MY_COUNTER_VISITED_TIMES_FILE_PATH, "utf8"))
+  if (fs.existsSync(VISIT_COUNTER_FILE_PATH)) {
+    pageVisitedTimesTotal = Number(
+      fs.readFileSync(VISIT_COUNTER_FILE_PATH, 'utf8')
+    );
   }
 } catch (e) {
   console.log(e);
 }
 
-app.get("/", (req, res) => {
-  ++pageVisitedTimesSession;
-  ++pageVisitedTimesTotal;
+app.get('/', (req, res) => {
+  pageVisitedTimesSession++;
+  pageVisitedTimesTotal++;
 
   res.status(200).send(
-    `This page was visited ${pageVisitedTimesTotal
-    } times in total, ${pageVisitedTimesSession} out of which was during the current code execution session ) `
+    `This page was visited ${pageVisitedTimesTotal} times in total,
+    ${pageVisitedTimesSession} out of which was during the current code execution session `
   );
 
   try {
-    fs.writeFileSync(MY_COUNTER_VISITED_TIMES_FILE_PATH, String(pageVisitedTimesTotal));
+    fs.writeFileSync(VISIT_COUNTER_FILE_PATH, String(pageVisitedTimesTotal));
   } catch (e) {
     console.log(e);
   }
