f9b5eec02be642696c4ed8843fc0ce6e37c56afa
Zhenyatta <jenya.paniryan8@gmail.com>
2023-01-25 14:35:01 +0400

    JSON added :poop:

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,45 @@
 import fs from 'fs';
 import express from 'express';
-
-const VISIT_COUNTER_FILE_PATH = 'temp/my-counter-visited-times.txt';
+import { VISIT_COUNTER_FILE_PATH } from './constants.js'
 
 const app = express();
 
 let pageVisitedTimesSession = 0;
-let pageVisitedTimesTotal = 0;
+
+let jsonObjForCounter = {
+  pageVisitedTimesTotal: 0,
+  pageVisitedTimesMaxDurSession: 0,
+};
 
 try {
   if (fs.existsSync(VISIT_COUNTER_FILE_PATH)) {
-    pageVisitedTimesTotal = Number(
-      fs.readFileSync(VISIT_COUNTER_FILE_PATH, 'utf8')
-    );
+    jsonObjForCounter = JSON.parse(fs.readFileSync(VISIT_COUNTER_FILE_PATH, "utf8"))
   }
 } catch (e) {
   console.log(e);
 }
 
-app.get('/', (req, res) => {
+app.get('/api/v1/my-counter', (req, res) => {
+
+  res.status(200).json(jsonObjForCounter);
+})
+
+app.get("/my-counter", (req, res) => {
   pageVisitedTimesSession++;
-  pageVisitedTimesTotal++;
+  jsonObjForCounter.pageVisitedTimesTotal++;
+
+  if (pageVisitedTimesSession > jsonObjForCounter.pageVisitedTimesMaxDurSession) {
+    jsonObjForCounter.pageVisitedTimesMaxDurSession = pageVisitedTimesSession;
+  }
 
   res.status(200).send(
-    `This page was visited ${pageVisitedTimesTotal} times in total,
-    ${pageVisitedTimesSession} out of which was during the current code execution session `
+    `This page was visited ${jsonObjForCounter.pageVisitedTimesTotal}
+    times in total, ${pageVisitedTimesSession} out of which was during the current code execution session.
+    Maximum page visits during a session ${jsonObjForCounter.pageVisitedTimesMaxDurSession} `
   );
 
   try {
-    fs.writeFileSync(VISIT_COUNTER_FILE_PATH, String(pageVisitedTimesTotal));
+    fs.writeFileSync(VISIT_COUNTER_FILE_PATH, JSON.stringify(jsonObjForCounter));
   } catch (e) {
     console.log(e);
   }
