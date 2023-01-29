e96dc057c07234f1ad5c2f8a4623f5ba7bc13fba
Zhenyatta <jenya.paniryan8@gmail.com>
2023-01-29 13:47:26 +0400

    improved code :art:

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,6 @@ app.get('/my-counter', (req, res) => {
   }
 });
 
-app.get('env', (req, res) => res.status(200).send(res.send(`ENV: ${process.env.ENV}`)));
+app.get('env', (req, res) => res.status(200).res.send(`ENV: ${process.env.ENV}`));
 
 app.listen(8080);
