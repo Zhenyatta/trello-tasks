35668a388ceb96cade054bf94fdabb361b389bae
Zhenya :) <79196310+Zhenyatta@users.noreply.github.com>
2023-02-02 10:38:43 +0400

    rebase problems :poop:

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-=======
->>>>>>> 2b78ac7 (react :poop:)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,11 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
->>>>>>> 2b78ac7 (react :poop:)
